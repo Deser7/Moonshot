74d659126eecfa9179d9c4517b96340db624cef2
Deser7 <deser7@icloud.com>
2025-07-18 18:57:19 +0400

    добавил в проект JSON и материалы, создал метод для работы с JSON

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,8 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Наташа Спиридонова on 18.07.2025.
+//
+
+import Foundation
---
+++ Moonshot/Bundle-Decodable.swift
@@ -0,0 +1,8 @@
+//
+//  Bundle-Decodable.swift
+//  Moonshot
+//
+//  Created by Наташа Спиридонова on 18.07.2025.
+//
+
+import Foundation
