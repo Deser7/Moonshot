361eefe5cf454372f25c2cbae182c38c01569b08
Deser7 <deser7@icloud.com>
2025-07-20 21:11:40 +0400

    добавил DateFormatter сделал первый экран

--- Moonshot/Bundle-Decodable.swift
+++ Moonshot/Bundle-Decodable.swift
@@ -10,7 +10,7 @@ import Foundation
 extension Bundle {
     func decode<T: Codable>(_ file: String) -> T {
         guard let url = self.url(forResource: file, withExtension: nil) else {
-            fatalError("Failed to lacate \(file) in bundle")
+            fatalError("Failed to locate \(file) in bundle")
         }
         
         guard let data = try? Data(contentsOf: url) else {
@@ -18,6 +18,9 @@ extension Bundle {
         }
         
         let decoder = JSONDecoder()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "y-MM-dd"
+        decoder.dateDecodingStrategy = .formatted(formatter)
 
         /*
         guard let loaded = try? decoder.decode([String: Astronaut].self, from: data) else {
--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,10 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
-    let mission: [Mission] = Bundle.main.decode("missions.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    let columns: [GridItem] = [
+        GridItem(.adaptive(minimum: 150))
+    ]
     
     var body: some View {
-        Text(astronauts.count.formatted())
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detail view")
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                    .padding()
+                                
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundStyle(
+                                            .white
+                                                .opacity(0.9)
+                                        )
+                                    
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundStyle(.gray)
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
+                        }
+                    }
+                }
+                .padding([.horizontal, .bottom])
+            }
+            .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
+        }
     }
 }
 
