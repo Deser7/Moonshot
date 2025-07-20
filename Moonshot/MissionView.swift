2f7cb45c8bd4d17024275c809554c0c4e4bc3804
Deser7 <deser7@icloud.com>
2025-07-20 21:27:42 +0400

    создал экран миссий с детальной информацией

---
+++ Moonshot/MissionView.swift
@@ -0,0 +1,18 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Наташа Спиридонова on 20.07.2025.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MissionView()
+}
